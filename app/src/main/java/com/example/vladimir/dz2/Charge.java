e2594cef137213342d371b3557585bd8b55651c5
VladimirAnic <vladimiranic@gmail.com>
2017-04-05 19:41:26 +0200

    layout completed

--- app/src/main/java/com/example/vladimir/dz2/Charge.java
+++ app/src/main/java/com/example/vladimir/dz2/Charge.java
@@ -2,12 +2,62 @@ package com.example.vladimir.dz2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class Charge extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Charge extends Activity implements View.OnClickListener {
+    Spinner spFrom, spTo;
+    EditText etCharge;
+    Button bCharge;
+    String spInput, spOutput, etConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charge);
+
+        setUpUI();
     }
+
+    private void setUpUI() {
+        this.spFrom = (Spinner) findViewById(R.id.spFrom);
+        this.spTo = (Spinner) findViewById(R.id.spTo);
+        this.etCharge = (EditText) findViewById(R.id.etCharge);
+        this.bCharge = (Button) findViewById(R.id.bCharge);
+
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.charge,
+                        android.R.layout.simple_spinner_item);
+
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spFrom.setAdapter(staticAdapter);
+
+
+
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spTo.setAdapter(staticAdapter);
+
+        this.bCharge.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+        spInput = spFrom.getSelectedItem().toString(); //pretvaranje spinnera u string
+        spOutput = spTo.getSelectedItem().toString();
+        etConvert = etCharge.getText().toString();
+
+        float Value = Float.parseFloat(etConvert);
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/vladimir/dz2/MainActivity.java
+++ app/src/main/java/com/example/vladimir/dz2/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.vladimir.dz2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    Button bCharge, bImageRes, bDistance, bTemperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setUpUI();
+    }
+
+    private void setUpUI() {
+        this.bCharge = (Button) findViewById(R.id.bCharge);
+        this.bImageRes = (Button) findViewById(R.id.bImageRes);
+        this.bDistance = (Button) findViewById(R.id.bDistance);
+        this.bTemperature = (Button) findViewById(R.id.bTemperature);
+
+        this.bCharge.setOnClickListener(this);
+        this.bImageRes.setOnClickListener(this);
+        this.bDistance.setOnClickListener(this);
+        this.bTemperature.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent explicitIntent = null;
+        switch (v.getId())
+        {
+            case(R.id.bCharge):
+                explicitIntent = new Intent(getApplicationContext(),Charge.class);
+                this.startActivity(explicitIntent);
+                break;
+            case(R.id.bImageRes):
+                explicitIntent = new Intent(getApplicationContext(),ImageResolution.class);
+                this.startActivity(explicitIntent);
+                break;
+            case(R.id.bDistance):
+                explicitIntent = new Intent(getApplicationContext(),Distance.class);
+                this.startActivity(explicitIntent);
+                break;
+            case(R.id.bTemperature):
+                explicitIntent = new Intent(getApplicationContext(),Temperature.class);
+                this.startActivity(explicitIntent);
+                break;
+
+        }
+
+
     }
 }
